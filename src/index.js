0a9ba2bcc478bec1c6103683fe8dc1d0a5ecb783
Jairo Diaz <jairo.diaz@codescrum.com>
2015-07-02 21:47:49 -0700

    Added movement with the mouse

--- src/index.js
+++ src/index.js
@@ -1,32 +1,37 @@
-var FamousEngine = require('famous/core/FamousEngine');
-var Mesh = require('famous/webgl-renderables/Mesh');
-var Material = require('famous/webgl-materials/Material');
-var Transitionable = require('famous/transitions/Transitionable');
+var famous = require('famous');
+
+var FamousEngine = famous.core.FamousEngine;
+var Mesh = famous.webglRenderables.Mesh;
+var Material = famous.webglMaterials.Material;
+var Position = famous.components.Position;
+var Transitionable = famous.transitions.Transitionable;
 
 var scene = FamousEngine.createScene('body');
 FamousEngine.init();
 
 var rootNode = scene.addChild();
-rootNode.setAlign(0.5,0.5, 0);
-rootNode.setOrigin(0.5, 0.5, 0);
-rootNode.setMountPoint(0.5, 0.5, 0.5);
+rootNode
+  .setAlign(0,0, 0)
+  .setOrigin(0.5, 0.5, 0.5)
+  .setMountPoint(0.5, 0.5, 0.5);
 
 // Add a child node to add our mesh to.
 var node = rootNode.addChild();
-node.setAlign(0.5,0.5, 0);
-node.setOrigin(0.5, 0.5, 0);
+node.setAlign(0.5, 0.5, 0.5);
+node.setOrigin(0.5, 0.5, 0.5);
 node.setMountPoint(0.5, 0.5, 0.5);
+var nodePosition = new Position(node);
 
 node.setSizeMode('absolute','absolute','absolute');
 node.setAbsoluteSize(200,200,200);
 
 // Start a Transitionable Rotation value
-var transitionY = new Transitionable();
-var milisecs = 10000;
-var startAngle = Math.PI * 2 / milisecs;
-function rotateY() {
-  transitionY.from(startAngle).set(Math.PI * 2, { duration: milisecs }, rotateY);
-}
+// var transition = new Transitionable([0, 0]);
+// var milisecs = 10000;
+// var startAngle = Math.PI * 2 / milisecs;
+// function rotateY() {
+//   transitionY.from(startAngle).set(Math.PI * 2, { duration: milisecs }, rotateY);
+// }
 
 // Pass child node into new Mesh component.
 var mesh = new Mesh(node);
@@ -36,14 +41,29 @@ mesh.setGeometry('Sphere', { detail: 100 });
 
 mesh.setBaseColor(Material.image([], { texture: 'https://i.imgur.com/xn7lVCw.png' }));
 
+document.addEventListener('mousemove', function (e) {
+  nodePosition.halt();
+  nodePosition.set(e.pageX, e.pageY, null, { duration: 400 });
+    // grav.anchor.set(e.pageX, e.pageY);
+    // node.setAlign(0, 0, 0);
+});
+
+var angle = Math.PI / 200;
+var rot = 0;
+var cir = Math.PI * 2;
 // Add a spinner component to the 'node' that is called, every frame
-var spinner = rootNode.addComponent({
+var spinner = node.addComponent({
   onUpdate: function(time) {
-    if (!transitionY.isActive()) rotateY();
-    rootNode.setRotation(0, transitionY.get(), 0);
-    rootNode.requestUpdateOnNextTick(spinner);
+    // if (!transitionY.isActive()) rotateY();
+    rot+=angle;
+    if (rot >= cir) {
+      rot = 0;
+    }
+    node.setRotation(0, rot, 0);
+    node.requestUpdateOnNextTick(spinner);
   }
 });
 
+
 // Let the magic begin...
-rootNode.requestUpdate(spinner);
+node.requestUpdate(spinner);
